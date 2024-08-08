03085bc7547c4b392965604e1b01e740be869b88
Alex Andrade <alexander.andrade@gmail.com>
2024-08-08 10:03:11 -0500

    📄 add documentation

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,25 @@ const { defineConfig } = require('cypress');
 const setupNodeEvents = require('./cypress/plugins/index');
 
 module.exports = defineConfig({
-    numTestsKeptInMemory: 0,
-    defaultCommandTimeout: 42000,
-    pageLoadTimeout: 300000,
-    responseTimeout: 60000,
-    requestTimeout: 405700,
-    chromeWebSecurity: false,
+    numTestsKeptInMemory: 0, // 0 = default behavior
+    // Standardize timeouts across most actions (consider adjusting as needed)
+    defaultCommandTimeout: 42000, // 42 seconds
+    pageLoadTimeout: 300000, // 30 minutes
+    responseTimeout: 60000, // 1 minute
+    requestTimeout: 405700, // +40 minutes
+
+    chromeWebSecurity: false, // Set to true for production
+
     env: {
         itemPrice: '',
     },
+
     e2e: {
         baseUrl: 'https://www.saucedemo.com',
         setupNodeEvents(on, config) {
-            return setupNodeEvents(on, config);
+            return setupNodeEvents(on, config); // Use the imported setupNodeEvents
         },
         specPattern: 'cypress/e2e/**/*.feature',
-        defaultCommandTimeout: 10000,
+        defaultCommandTimeout: 10000, // 10 seconds, override the global default
     },
 });
