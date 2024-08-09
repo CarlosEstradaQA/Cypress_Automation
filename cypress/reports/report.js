1c768524d23088fcc120839f7ec88abdb27b3d3b
Carlos Armando Estrada Torres <carlos.estrada@blankfactor.com>
2024-08-09 15:26:57 -0500

    Implement Multiple Cucumber HTML Reporter and adjust some files to properly work

---
+++ cypress/reports/report.js
@@ -0,0 +1,24 @@
+const report = require('multiple-cucumber-html-reporter');
+const moment = require('moment');
+
+report.generate({
+    jsonDir: './cypress/cucumber-json',
+    reportPath: './cypress/cucumber-report',
+    screenshotsFolder: 'reports/cypress/screenshots',
+    ignoreBadJsonFile: true,
+    durationInM: true,
+    displayDuration: true,
+    reportName: 'Cypress automation with Cucumber over SouceDemo',
+    pageTitle: 'Automation Report',
+    displayReportTime: true,
+
+    customData: {
+        title: 'Run info',
+        data: [
+            { label: 'Project', value: 'Sauce Demo' },
+            { label: 'Release', value: '1.0.0' },
+            { label: 'Environment', value: 'Pre-production' },
+            { label: 'Execution End Time', value: moment().format('YYYY-MM-DD, HH:mm:ss A') },
+        ],
+    },
+});
